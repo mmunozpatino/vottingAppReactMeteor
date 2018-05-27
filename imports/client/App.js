d29f438ef67ce80fc40b022075e51769273841a8
mmunozpatino <mercedesmp1305@gmail.com>
2018-05-27 12:43:09 -0300

    ver tema login

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,39 +1,58 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
+import Item from './Item';
+import Items from '../api/Items.js';
 // let hello = "Hello Mechi!";
 
 // let headdingClick = function() {
 //     console.log('hello');
 // }
 
-export default class App extends Component {
-    constructor(){
+class App extends Component {
+    constructor() {
         super();
         this.state = {
             count: 0
         };
     }
-    headdingClick(){
+    headdingClick() {
         // console.log('hello')
-        this.setState({count: this.state.count + 1});
+        this.setState({ count: this.state.count + 1 });
     }
-    render(){
-        return(
-            // bind es para que el this. sea válido en la funcion
-            <header onClick={this.headdingClick.bind(this)}>
-                {/* {this.state.count} */}
-                <Heading 
-                    count = {this.state.count}
-                />
-            </header>
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1>Level Up Voting</h1>
+                    <LoginButtons />
+                    <button onClick={this.showAll.bind(this)}>
+                        Show {this.props.showAll ? 'One' : 'All'}
+                    </button>
+                </header>
+                <main>
+                    <form className='new-items' onSubmit={this.addItems.bind(this)}>
+                        <input type='text' ref='itemOne' />
+                        <input type='text' ref='itemTwo' />
+                        <button type='submit'>Add Items</button>
+                    </form>
+                    {this.props.items.map((item) => {
+                        return <Item item={item} key={item._id} />
+                    })}
+                </main>
+            </div>
         );
     }
 }
-
-class Heading extends Component{
-    render(){
-        return(
-            <h1>{this.props.count}</h1>
-        )
+export default withTracker(() => {
+    let itemsSub = Meteor.subscribe('allItems');
+    let showAll = Session.get('showAll');
+    return {
+        showAll,
+        ready: itemsSub.ready(),
+        items: Items.find({}, {
+            limit: showAll ? 50 : 1,
+            sort: { lastUpdated: 1 }
+        }).fetch()
     }
-}
+})(App);
